d76647e485e83a469ecb5d79dd26999189412609
Kyxie <chris_xiie@outlook.com>
2021-04-18 10:16:28 +0800

    Finish UESTC

--- others/cmc.py
+++ others/cmc.py
@@ -6,22 +6,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 rank = [1, 5, 10, 20]
-market1501 = [86.0, 91.8, 93.7, 95.9]
+market1501 = [76.3, 91.8, 93.7, 95.9]
+UESTC = [80.0, 93.3, 100.0, 100.0]
 
 def cmc(hor, ver, data):
+    plt.figure('{}'.format(data))
     plt.plot(hor, ver)
     plt.xlabel('rank@')
     plt.ylabel('%')
     plt.title("CMC Curve of {}".format(data))
     my_x_ticks = rank
-    my_y_ticks = np.arange(80, 100, 2)
+    my_y_ticks = np.arange(75, 100, 2)
     plt.xticks(my_x_ticks)
     plt.yticks(my_y_ticks)
     for i in range(0, 4):
-        plt.annotate("{}%".format(ver[i]), xy=(hor[i], ver[i]), xytext=(hor[i]+1, ver[i]-2), arrowprops=dict(facecolor='black', shrink=0.05))
+        plt.annotate("{}%".format(ver[i]), xy=(hor[i], ver[i]), xytext=(hor[i]+1, ver[i]-3), arrowprops=dict(facecolor='black', shrink=0.05))
     plt.grid()
     plt.show()
 
 if __name__ == '__main__':
-    plt.figure('Market1501')
     cmc(rank, market1501, 'Market1501')
+    cmc(rank, UESTC, 'UESTC')
